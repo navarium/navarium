6c9b294754fb2023271ed539641cf96c032f7254
Gabor Loth <lgabster@gmail.com>
2023-03-03 13:08:27 +0100

    remove unused component

--- src/main.js
+++ src/main.js
@@ -1,15 +1,6 @@
-// This is the main.js file. Import global CSS and scripts here.
-// The Client API can be used here. Learn more: gridsome.org/docs/client-api
-
 import DefaultLayout from '~/layouts/Default.vue'
 
-import VueScrollactive from "vue-scrollactive"
-
-
-
 export default function (Vue, { router, head, isClient }) {
-  Vue.use(VueScrollactive)
-  // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 
   head.link.push({
